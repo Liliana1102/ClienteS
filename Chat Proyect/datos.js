b59ef16bbd838808315cbcdabaf400a6f2ac01a9
Liliana <211108@ids.upchiapas.edu.mx>
2023-01-28 21:39:54 -0600

    mi primer commit

---
+++ Chat Proyect/datos.js
@@ -0,0 +1,20 @@
+// let nombre = 'Liliana';
+// let edad = 20;
+
+// let nombreEdad = nombre + ' ' + edad;
+// console.log(nombreEdad);
+// console.log(typeof nombreEdad);
+
+
+let vueltas = 7;
+
+for (let i = 0;i * vueltas; i++){
+    let dato = 7;
+    console.log(7);
+
+}
+
+
+
+
+
